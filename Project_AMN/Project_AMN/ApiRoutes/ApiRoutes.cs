d926242861e40581fe28e094ff979d180b3db713
Alejandrito8 <aleksandar.jurkovic@edu.newton.se>
2025-09-04 19:17:49 +0200

    Add API routes for articles and implement order service methods; include example data and comments for future development

--- Project_AMN/Project_AMN/ApiRoutes/ApiRoutes.cs
+++ Project_AMN/Project_AMN/ApiRoutes/ApiRoutes.cs
@@ -1,14 +1,16 @@
 
 namespace Project_AMN.ApiRoutes;
+
 public static class ApiRoutes
 {
     public static IEndpointRouteBuilder MapApiRoutes(this IEndpointRouteBuilder endpoints)
     {
         endpoints.MapGet(pattern: "/api/articles", () => new[]
         {
+            //Test data
             new { id = 1, name = "Skruv", sku = "A-001", stock = 100, location = "A1" },
             new { id = 2, name = "Spik",  sku = "A-201", stock = 2300, location = "B4" }
-        })        .WithName("GetArticles")
+        }).WithName("GetArticles")
             .WithTags("Articles")
             .WithMetadata(new Microsoft.AspNetCore.Authorization.AllowAnonymousAttribute())
             .Produces(statusCode: StatusCodes.Status200OK)
@@ -16,5 +18,7 @@ public static class ApiRoutes
             .Produces(statusCode: StatusCodes.Status403Forbidden);
         return endpoints;
     }
+    
+    // OBS, detta är bara en exempelroute, ta bort denna fil när ni skapar era egna routes
 
 }               
