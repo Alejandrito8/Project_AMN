989a20b9c4b2590317b0d12d2ba1f6b655eacc43
Adonai Yohannes <adonai.yohannes@gmail.com>
2025-09-18 21:14:20 +0200

    Refactor authorization setup: Simplify role policies and streamline role initialization for Admin and User roles.

--- Project_AMN/Project_AMN/Program.cs
+++ Project_AMN/Project_AMN/Program.cs
@@ -59,19 +59,9 @@ builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddAuthorization(options =>
-{
-    foreach (var role in RoleDefinitions.RolePermission.Keys)
-    {
-        options.AddPolicy($"{role}Policy", policy => policy.RequireRole(role));
-    }
-});
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
-
-
-
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -121,17 +111,23 @@ async Task InitializeRolesAndAdmin(WebApplication app)
     using var scope = app.Services.CreateScope();
     var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
     var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
-
-
-    foreach (var role in RoleDefinitions.RolePermission.Keys)
+ 
+    var roles = new[] { "Admin", "User" };
+    foreach (var role in roles)
     {
         if (!await roleManager.RoleExistsAsync(role))
         {
             await roleManager.CreateAsync(new IdentityRole(role));
         }
     }
-
+ 
     string email = "admin@admin.se";
     string password = "Abc123!";
-
+ 
+    if (await userManager.FindByEmailAsync(email) == null)
+    {
+        var user = new ApplicationUser { UserName = email, Email = email };
+        await userManager.CreateAsync(user, password);
+        await userManager.AddToRoleAsync(user, "Admin");
+    }
 }
